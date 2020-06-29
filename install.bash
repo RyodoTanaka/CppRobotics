f8f2e0e29a2ca2c3b29032279f76d01114edeac6
RyodoTanaka <groadpg@gmail.com>
2020-06-29 22:20:26 +0900

    Update to add basic examples

---
+++ install.bash
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo apt -y install libeigen3-dev qt5-default
---
+++ src/Qt5/Introduction/version.cpp
@@ -0,0 +1,10 @@
+#include <QtCore>
+#include <iostream>
+
+int main(int argc, char** argv) {
+  using namespace std;
+
+  cout << "Qt version : " << qVersion() << endl;
+
+  return 0;
+}
