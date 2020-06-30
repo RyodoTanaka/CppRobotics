5b6f505c081fa6302ac54d23339be44c6c1578e3
RyodoTanaka <groadpg@gmail.com>
2020-06-30 20:29:13 +0900

    Update to add plot example

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+// Eigen //
+#include <Eigen/Core>
+// Qt5 //
+#include <QApplication>
+// Original //
+#include <plot.h>
+
+int main(int argc, char *argv[])
+{
+  // need to initialize Qt //
+  QApplication app(argc, argv);
+
+  // 2d plotting //
+  CppRobotics::Plot2D plt;
+
+  for(uint i=0; i<10; i++) {
+    Eigen::Vector2d p = 10.0*Eigen::Vector2d::Random();
+    plt.plotPoint(p);
+  }
+
+  plt.show();
+  
+  return app.exec();
+}
